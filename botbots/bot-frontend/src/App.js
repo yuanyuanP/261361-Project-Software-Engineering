00dc31b59449f5d04f157189b6687ea93a950bc6
isne919 <paripat_y@cmu.ac.th>
2023-09-13 02:06:29 +0700

    Update the new one

--- botbots/bot-frontend/src/App.js
+++ botbots/bot-frontend/src/App.js
@@ -2,25 +2,30 @@ const App = () => {
   return (
     <div className="app">
       <section className="side-bar">
-        <button> + New Chat</button>
+        <button>+ New Chat</button>
         <ul className="history">
-          <li>aaa</li>
+          <li>Eww</li>
         </ul>
-        <nav>BotBots</nav>
+        <nav>
+          <p>Made By BotBots Team</p>
+        </nav>
       </section>
+
       <section className="main">
         <h1>BotBots</h1>
         <ul className="feed"></ul>
         <div className="bottom-section">
           <div className="input-container">
             <input />
-            <div id="submit"> | </div>
+            <div id="submit">➢</div>
           </div>
-          <p className="info">aaaaaaaaaaaaaaaaaaaaaaaa</p>
+
+          <p className="info">This is botbots ....</p>
+          
         </div>
       </section>
     </div>
   );
-};
+}
 
 export default App;
